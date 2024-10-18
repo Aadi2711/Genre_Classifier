1a7ca8d7d3e6b1906db105c94356b7fcaa1b5b24
Aaditya Gupta <aadityagupta2711@gmail.com>
2024-10-19 00:01:54 +0530

    Path Fix

--- Music_Genre_App.py
+++ Music_Genre_App.py
@@ -6,42 +6,32 @@ from matplotlib import pyplot
 import numpy as np
 from tensorflow import image
 
-#Function
 @st.cache_resource()
 def load_model():
   model = tf.keras.models.load_model("Trained_model.h5")
   return model
 
 
-# Load and preprocess audio data
-def load_and_preprocess_data(file_path, target_shape=(150, 150)):
+def load_and_preprocess_data(test_mp3, target_shape=(150, 150)):
     data = []
-    audio_data, sample_rate = librosa.load(file_path, sr=None)
-    # Perform preprocessing (e.g., convert to Mel spectrogram and resize)
-    # Define the duration of each chunk and overlap
-    chunk_duration = 4  # seconds
-    overlap_duration = 2  # seconds
+    audio_data, sample_rate = librosa.load(test_mp3)
+
+    chunk_duration = 4
+    overlap_duration = 2
                 
-    # Convert durations to samples
     chunk_samples = chunk_duration * sample_rate
     overlap_samples = overlap_duration * sample_rate
                 
-    # Calculate the number of chunks
     num_chunks = int(np.ceil((len(audio_data) - chunk_samples) / (chunk_samples - overlap_samples))) + 1
                 
-    # Iterate over each chunk
     for i in range(num_chunks):
-                    # Calculate start and end indices of the chunk
         start = i * (chunk_samples - overlap_samples)
         end = start + chunk_samples
                     
-                    # Extract the chunk of audio
         chunk = audio_data[start:end]
                     
-                    # Compute the Mel spectrogram for the chunk
         mel_spectrogram = librosa.feature.melspectrogram(y=chunk, sr=sample_rate)
                     
-                #mel_spectrogram = librosa.feature.melspectrogram(y=audio_data, sr=sample_rate)
         mel_spectrogram = image.resize(np.expand_dims(mel_spectrogram, axis=-1), target_shape)
         data.append(mel_spectrogram)
     
@@ -49,13 +39,11 @@ def load_and_preprocess_data(file_path, target_shape=(150, 150)):
 
 
 
-#Tensorflow Model Prediction
 def model_prediction(X_test):
     model = load_model()
     y_pred = model.predict(X_test)
     predicted_categories = np.argmax(y_pred,axis=1)
     unique_elements, counts = np.unique(predicted_categories, return_counts=True)
-    #print(unique_elements, counts)
     max_count = np.max(counts)
     max_elements = unique_elements[counts == max_count]
     return max_elements[0]
@@ -131,18 +119,16 @@ elif(app_mode=="About Project"):
 elif(app_mode=="Prediction"):
     st.header("Model Prediction")
     test_mp3 = st.file_uploader("Upload an audio file", type=["mp3"])
-    if test_mp3 is not None:
-            filepath = 'Test_Music/'+test_mp3.name
+    # if test_mp3 is not None:
+    #         filepath = 'Test_Music/'+test_mp3.name
             
 
-    #Show Button
     if(st.button("Play Audio")):
         st.audio(test_mp3)
     
-    #Predict Button
     if(st.button("Predict")):
       with st.spinner("Please Wait.."):       
-        X_test = load_and_preprocess_data(filepath)
+        X_test = load_and_preprocess_data(test_mp3)
         result_index = model_prediction(X_test)
         st.balloons()
         label = ['blues', 'classical','country','disco','hiphop','jazz','metal','pop','reggae','rock']
